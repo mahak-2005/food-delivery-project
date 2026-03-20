0a9c8e51e3a93b1e2fcaf84eba4bc23ddef69ddd
kundan kumar sahani <kundankumarsahani@Mac.mshome.net>
2026-03-20 14:02:54 +0530

    Update teh Navbar folder

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -17,9 +17,6 @@ const FoodItem = ({ id, name, price, description, image }) => {
             <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
             <p>{cartItems[id]}</p>
             <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
-
-
-
           </div>
 
         }
@@ -30,7 +27,7 @@ const FoodItem = ({ id, name, price, description, image }) => {
           <img src={assets.rating_starts} alt="" />
         </div>
         <p className="food-item-desc">{description}</p>
-        <p className="food-item-price">${price}</p>
+        <p className="food-item-price">₹{price}</p>
       </div>
     </div>
   )
