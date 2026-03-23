5d24d7a5f48c4f9b3413b7543ea68ebdd411ee6c
mahak-2005 <mahakgupta177@gmail,com>
2026-03-23 23:36:18 +0530

    admin panel changes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import { Routes, Route } from "react-router-dom";
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Order from './pages/Orders/Order'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
     <div>
-      
+      <ToastContainer/>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/" element={<Add/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/order" element={<Order/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
