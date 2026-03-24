ebe7709982b44b919a7d803b6f34a727f97edc26
mahak-2005 <mahakgupta177@gmail,com>
2026-03-25 01:20:37 +0530

    update the features

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,11 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js"; // ✅ FIXED
+import dotenv from "dotenv";
 
-// app config
+dotenv.config();
+
 const app = express();
 const port = 4000;
 
@@ -14,10 +17,10 @@ app.use(cors());
 // db connection
 connectDB();
 
-// api endpoints
-app.use("/api/food",foodRouter)
-app.use("/images",express.static('uploads'))
-
+// routes
+app.use("/api/food", foodRouter);
+app.use("/api/user", userRouter);
+app.use("/images", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("API Working");
