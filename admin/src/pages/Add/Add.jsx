962048df5bad7e997af64431c7d4e7f03ade94b8
mahak-2005 <mahakgupta177@gmail,com>
2026-03-25 01:21:16 +0530

    update the features

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -6,9 +6,9 @@ import axios from "axios"
 import { toast } from 'react-toastify'
 
 
-const Add = () => {
+const Add = ({url}) => {
 
-    const url = "http://localhost:4000";
+    
 
     const [image,setImage] = useState(false);
     const [data,setData] = useState({
